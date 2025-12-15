cf89655d0a3659fc9ea3397007aafe7f0c0f5a24
sadique <sadique1492@gmail.com>
2025-12-15 23:33:06 +0530

    test commit

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -2,8 +2,7 @@ const{test, expect} = require('@playwright/test');
 
 test.only('first playwright test',async ({browser})=>
     
-{
-   
+{ 
     const context = await browser.newContext();
     const page = await context.newPage();
     const errormessage = page.locator("[style*='block']");
